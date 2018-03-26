d9a030f31cc4d9c43bf45ddb0a4907f6f5cc79ea
Giuseppe Monetti <monygioo.gm@gmail.com>
2018-03-26 15:29:10 +0200

    Ho impostato i riferimenti del layout

--- app/src/main/java/e/giuseppemonetti/contatore/MainActivity.java
+++ app/src/main/java/e/giuseppemonetti/contatore/MainActivity.java
@@ -2,12 +2,28 @@ package e.giuseppemonetti.contatore;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    //LAYOUT'S VIEW
+    private TextView vMessage;
+    private Button vAdd;
+    private Button vReduce;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //RIFERIMENTI LAYOUT
+        vMessage = findViewById(R.id.textMessage);
+        vAdd = findViewById(R.id.buttonAdd);
+        vReduce = findViewById(R.id.buttonReduce);
+
+        
     }
 }
